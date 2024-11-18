62e36ce1c85b030b23e406becd170f294298daf0
Dinh Pham <phamducdinh042004@gmail.com>
2024-11-18 15:14:42 +0700

    khoi tao du an quan ly khach san

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,19 +1,19 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('DbCuaDinh', 'root', null, {
+const sequelize = new Sequelize('hoteldb', 'root', null, {
   host: 'localhost', // địa chỉ server 
-  dialect:'mysql', 
+  dialect: 'mysql',
   logging: false, // loai bo dong log 
 });
 
 
 let connectDB = async () => {
   try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
   }
 }
 
